bd6e2bdf46f689e9d1dd22345cf6929ce990845a
Adrienne Wootton <adrienne@Owners-MacBook-Pro.local>
2019-11-13 16:32:37 -0700

    react-app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+
+  constructor(){
+    super();
+    this.state = {
+      message: ""
+    };
+  }
+
+  //this is the function or 'event' your trying to do
+  //then value for whatever the value of the change is
+handleChange(value){
+  //then this.setState is basically 'resetting' whatever th value WAS
+  this.setState({
+    message: value
+  });
+}
+
+
   render() {
+
+  
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <input onChange={e => this.handleChange(e.target.value)} type="text" />
+
+    {/* The 'input' is referring to whatever is being put in
+    The 'type' is whatever type of data: text, picture, audio, etc.
+    The 'onChange' is whatever we want to have happen once they di that 'action'
+    The 'e' - is the 'event' or 'action' that is going to happen
+    The 'target' is referring to the input (or whatever is getting put in)
+    The 'value' is referring to whatever the value of that is  */}
+      <p>{this.state.message}</p>
       </div>
     );
   }
